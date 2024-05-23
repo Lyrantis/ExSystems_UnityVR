333bef0bf714efbc6ff6a8b9ad9b6108026a2af8
BenEdwards-0612 <112256392+BenEdwards-0612@users.noreply.github.com>
2024-05-23 03:12:32 +0100

    Pipe Puzzle generation done
    Need to implement base start + open end node.
    
    Need to allow pipes to be connected

--- Assets/Scripts/Puzzles/PipePuzzle/PipeWall.cs
+++ Assets/Scripts/Puzzles/PipePuzzle/PipeWall.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.ProBuilder;
 
-public struct Node
+public class Node
 {
     public Vector2 positionOnGrid;
     public List<Node> neighbours;
@@ -15,16 +16,51 @@ public struct Node
         neighbours = new List<Node>();
         neighbourCosts = new List<int>();
     }
+}
 
+public class NodeInformation
+{
+    public Node node;
+    public NodeInformation parent;
+    public float gCost, hCost, fCost;
+
+    public NodeInformation(Node node, NodeInformation parentToAdd, float gCost, int maxX, int maxY)
+    {
+        this.node = node;
+        this.parent = parentToAdd;
+        this.gCost = gCost;
+        GenerateHeuristic(maxX, maxY);
+        this.fCost = gCost + hCost;
+    }
+
+    private void GenerateHeuristic(int maxX, int maxY)
+    {
+        hCost = (maxX - node.positionOnGrid.x) + (maxY - node.positionOnGrid.y);
+    }
 }
 
 public class PipeWall : MonoBehaviour
 {
     [SerializeField]
+    public GameObject puzzleStartCorner;
+
+    [SerializeField]
     public int PuzzleWidth = 5;
     
     [SerializeField]
     public int PuzzleHeight = 5;
+
+    [SerializeField]
+    public GameObject straightPipe;
+    
+    [SerializeField]
+    public GameObject LPipe;
+
+    [SerializeField]
+    public GameObject TPipe;
+
+    [SerializeField]
+    public GameObject XPipe;
 
     private List<List<Node>> grid = new List<List<Node>>();
 
@@ -35,7 +71,7 @@ public class PipeWall : MonoBehaviour
             grid.Add(new List<Node>());
             for (int j = 0; j < PuzzleWidth; j++)
             {
-                grid[i].Add(new Node(new Vector2(i, j)));
+                grid[i].Add(new Node(new Vector2(j, i)));
             }
         }
 
@@ -44,6 +80,12 @@ public class PipeWall : MonoBehaviour
             grid.Add(new List<Node>());
             for (int j = 0; j < PuzzleWidth; j++)
             {
+                if (i == j & j == 0)
+                {
+                    grid[i][j].neighbours.Add(grid[i][j + 1]);
+                    grid[i][j].neighbourCosts.Add(UnityEngine.Random.Range(1, 4));
+                    continue;
+                }
                 if (i > 0)
                 {
                     grid[i][j].neighbours.Add(grid[i - 1][j]);
@@ -70,21 +112,184 @@ public class PipeWall : MonoBehaviour
 
     private void GeneratePuzzle()
     {
-        List<Node> OpenList = new List<Node>();
-        List<Node> ClosedList = new List<Node>();
-        Node current = grid[0][0];
-        Node end = grid[PuzzleHeight][PuzzleWidth];
+
+        GridSetup();
+
+        List<NodeInformation> OpenList = new List<NodeInformation>();
+        List<NodeInformation> ClosedList = new List<NodeInformation>();
+        NodeInformation current = null;
+        Node end = grid[PuzzleHeight - 1][PuzzleWidth - 1];
         bool pathFound = false;
+
+        current = new NodeInformation(grid[0][0], null, 0.0f, PuzzleWidth, PuzzleHeight);
 
         while (!pathFound)
         {
+            if (current.node == end)
+            {
+                pathFound = true;
 
+                List<List<int>> pipeGridOutput = new List<List<int>>();
+
+                for (int i = 0; i <PuzzleHeight; i++)
+                {
+                    pipeGridOutput.Add(new List<int>());
+
+                    for (int j = 0; j <PuzzleWidth; j++)
+                    {
+                        pipeGridOutput[i].Add(0);
+                    }
+                }
+
+                while (current.parent.parent != null)
+                {
+                    if (Mathf.Abs(current.node.positionOnGrid.x - current.parent.parent.node.positionOnGrid.x) == 2 || Mathf.Abs(current.node.positionOnGrid.y - current.parent.parent.node.positionOnGrid.y) == 2)
+                    {
+                        pipeGridOutput[(int)current.parent.node.positionOnGrid.y][(int)current.parent.node.positionOnGrid.x] = 1;
+                    }
+                    else
+                    {
+                        pipeGridOutput[(int)current.parent.node.positionOnGrid.y][(int)current.parent.node.positionOnGrid.x] = 2;
+                    }
+                    current = current.parent;
+                }
+                for (int y = 0; y < pipeGridOutput.Count; y++)
+                {
+                    for (int x = 0; x < pipeGridOutput[y].Count; x++)
+                    {
+                        if ((x == y & y == 0) || (x == y & y == 4))
+                        {
+                            continue;
+                        }
+                        GameObject temp = new GameObject();
+
+                        if (pipeGridOutput[y][x] == 0)
+                        {
+                            int prefabToSpawn = UnityEngine.Random.Range(1, 5);
+                            
+                            switch (prefabToSpawn)
+                            {
+                                case 0:
+                                    Debug.Log("Error: 0");
+                                    break;
+                                case 1:
+                                    temp = straightPipe;
+                                    break;
+                                case 2:
+                                    temp = LPipe;
+                                    break;
+                                case 3:
+                                    temp = TPipe;
+                                    break;
+                                case 4:
+                                    temp = XPipe;
+                                    break;
+                                default:
+                                    Debug.Log("Unrecognised Symbol");
+                                    break;
+
+                            }
+                        }
+                        else
+                        {
+                            float randomChance = UnityEngine.Random.Range(0.0f, 1.0f);
+
+                            if (randomChance < 0.2f)
+                            {
+                                int prefabToSpawn = UnityEngine.Random.Range(3, 5);
+
+                                switch (prefabToSpawn)
+                                {
+                                    case 3:
+                                        temp = TPipe;
+                                        break;
+                                    case 4:
+                                        temp = XPipe;
+                                        break;
+                                    default:
+                                        Debug.Log("Unrecognised Symbol");
+                                        break;
+                                }
+                            }
+                            else
+                            {
+                                switch (pipeGridOutput[y][x])
+                                {
+                                    case 1:
+                                        temp = straightPipe;
+                                        break;
+                                    case 2:
+                                        temp = LPipe;
+                                        break;
+                                    default:
+                                        Debug.Log("Unrecognised Symbol");
+                                        break;
+                                }
+                            }
+                        }
+                        Vector3 offset = new Vector3(-1.0f * 0.3f * x, -1.0f * 0.3f * y, 0.0f);
+                        Instantiate(temp, puzzleStartCorner.transform.position + offset, Quaternion.identity, null);
+                    }
+                }
+            }
+
+            for (int i = 0; i < current.node.neighbours.Count; i++)
+            {
+                NodeInformation neighbour = new NodeInformation(current.node.neighbours[i], current, current.gCost + current.node.neighbourCosts[i], PuzzleWidth, PuzzleHeight);
+
+                if (OpenList.Contains(neighbour))
+                {
+                    int foundIndex = OpenList.IndexOf(neighbour);
+                    if (foundIndex >= 0)
+                    {
+                        if (OpenList[foundIndex].gCost > neighbour.gCost)
+                        {
+                            OpenList[foundIndex] = neighbour;
+                        }
+                    }
+                }
+                else if (ClosedList.Contains(neighbour))
+                {
+                    int foundIndex = ClosedList.IndexOf(neighbour);
+                    if (foundIndex >= 0)
+                    {
+                        if (ClosedList[foundIndex].gCost > neighbour.gCost)
+                        {
+                            ClosedList[foundIndex] = neighbour;
+                        }
+                    }
+                }
+                else
+                {
+                    OpenList.Add(neighbour);
+                }
+            }
+
+            OpenList.Remove(current);
+            ClosedList.Add(current);
+
+            if (OpenList.Count == 0)
+            {
+                Debug.Log("Failed - Open List is Empty");
+                break;
+            }
+            else
+            {
+                current = OpenList[0];
+
+                for (int i = 0; i < OpenList.Count - 1; i++)
+                {
+                    if (OpenList[i + 1].gCost < current.gCost)
+                    {
+                        current = OpenList[i + 1];
+                    }
+                }
+            }
         }
-        //Start at [0][0]
-        //End at [Height][Width]
-        //A* pathfindng
-        //Iterate through path, assigning appropriate pipe pieces for connection type (T's and +'s can be added anywhere, randomly)
-        //Fill rest of grid with random pipes 
+    }
 
+    private void Start()
+    {
+        GeneratePuzzle();
     }
 }
